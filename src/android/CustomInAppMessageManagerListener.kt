16dd0bd3511211fd6d4c5ff185ef169db5c6dfb2
Kirsten Frager <kirsten@hellostake.com>
2024-01-25 10:49:01 +1000

    Merge branch 'master' of https://github.com/braze-inc/braze-cordova-sdk into braze-inc-master
    # Conflicts:
    #	plugin.xml
    #	src/android/AppboyPlugin.java
    #	src/ios/AppboyPlugin.h
    #	src/ios/AppboyPlugin.m
    #	www/BrazePlugin.js

---
+++ src/android/CustomInAppMessageManagerListener.kt
@@ -0,0 +1,30 @@
+// // TODO: this needs to be converted over to .kt and integrated
+
+// package com.appboy.cordova;
+
+// import android.app.Activity;
+// import com.braze.ui.inappmessage.InAppMessageOperation;
+// import com.braze.ui.inappmessage.listeners.IInAppMessageManagerListener;
+// import android.util.Log;
+// import com.braze.models.inappmessage.IInAppMessage;
+
+// public class CustomInAppMessageManagerListener implements IInAppMessageManagerListener {
+//   private final Activity mActivity;
+//   public int inAppDisplayAttempts = 0;
+//   private static final String TAG = "BrazeCordova";
+
+//   public CustomInAppMessageManagerListener(Activity activity) {
+//     mActivity = activity;
+//   }
+
+//   @Override
+//   public InAppMessageOperation beforeInAppMessageDisplayed(IInAppMessage inAppMessage) {
+//     if (this.inAppDisplayAttempts >= 1) {
+//       Log.i(TAG, "Set in-app to display now");
+//       return InAppMessageOperation.DISPLAY_NOW;
+//     } else {
+//       Log.i(TAG, "Set in-app to display now");
+//       return InAppMessageOperation.DISPLAY_LATER;
+//     }
+//   }
+// }
